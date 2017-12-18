bdca7b88381ae01262a43f8b052bf41eff5f48d5
shansfk <ksrajbe@gmail.com>
2017-12-18 23:15:18 +0530

    Added sidebar.

--- ui/src/components/home/Home.js
+++ ui/src/components/home/Home.js
@@ -1,11 +1,26 @@
 import React, { Component } from "react";
+import { Grid } from "semantic-ui-react";
+
 import Header from "./header/Header";
+import Sidebar from "./sidebar/Sidebar";
+
+import "./home.css";
 
 class Home extends Component {
   render() {
     return (
       <div>
         <Header />
+        <Grid columns={2} divided="vertically">
+          <Grid.Row id="container">
+            <Grid.Column width={3} style={{ marginBottom: 0 }}>
+              <Sidebar />
+            </Grid.Column>
+            <Grid.Column width={13} style={{ marginBottom: 0 }}>
+              Content goes here
+            </Grid.Column>
+          </Grid.Row>
+        </Grid>
       </div>
     );
   }
---
+++ ui/src/components/home/sidebar/Sidebar.js
@@ -0,0 +1,48 @@
+import React, { Component } from "react";
+import { Menu, Icon } from "semantic-ui-react";
+import "./sidebar.css";
+
+export default class Sidebar extends Component {
+  state = {};
+
+  handleItemClick = (e, { name }) => this.setState({ activeItem: name });
+
+  render() {
+    const { activeItem } = this.state;
+
+    return (
+      <Menu fluid vertical id="menu">
+        <Menu.Item
+          name="settings"
+          active={activeItem === "settings"}
+          onClick={this.handleItemClick}
+        >
+          <div>
+            <Icon name="settings" size="large" />
+            Settings
+          </div>
+        </Menu.Item>
+        <Menu.Item
+          name="customers"
+          active={activeItem === "customers"}
+          onClick={this.handleItemClick}
+        >
+          <div>
+            <Icon name="address book" size="large" />
+            Customers
+          </div>
+        </Menu.Item>
+        <Menu.Item
+          name="pos"
+          active={activeItem === "pos"}
+          onClick={this.handleItemClick}
+        >
+          <div>
+            <Icon name="shopping cart" size="large" />
+            Point Of sale
+          </div>
+        </Menu.Item>
+      </Menu>
+    );
+  }
+}
