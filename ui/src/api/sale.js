1288054942df25631ebad3f39f9043284712b7ea
shansfk <ksrajbe@gmail.com>
2018-02-27 18:09:05 +0530

    Updated sale with trycatch.

--- ui/src/api/sale.js
+++ ui/src/api/sale.js
@@ -2,21 +2,33 @@ import axios from "axios";
 
 export default {
   getTransId: async () => {
-    const res = await axios.get("api/sale/transId");
-    return res.data.payload;
+    try {
+      const res = await axios.get("api/sale/transId");
+      return res.data.payload;
+    } catch (error) {
+      return { error: error.message }
+    }
   },
 
   addItemToCart: async item => {
-    const t = {};
-    Object.assign(t, item);
-    delete t.netPrice;
-    delete t.pricePerQty;
-    const res = await axios.post("api/sale", t);
-    return res.data.payload;
+    try {
+      const t = {};
+      Object.assign(t, item);
+      delete t.netPrice;
+      delete t.pricePerQty;
+      const response = await axios.post("api/sale", t);
+      return response.data.payload;
+    } catch (error) {
+      return { error: error.message }
+    }
   },
 
   emptyCart: async transId => {
-    const res = await axios.delete("api/sale", { data: { transId } });
-    return res.data.payload;
+    try {
+      const response = await axios.delete("api/sale", { data: { transId } });
+      return response.data.payload;
+    } catch (error) {
+      return { error: error.message }
+    }
   }
 };
